72a470ffb549a993325741ae671abee83792e0e4
Di Yuan <dyuan@thoughtworks.com>
2018-07-18 16:15:43 +0800

    [Yuan Di] task4 - game end after 6 chances or guess the correct number

--- main.js
+++ main.js
@@ -2,19 +2,33 @@
 
 const ERROR_MESSAGE = "Wrong Input，Input again";
 const answer = generateAnswer();
+let chances = 6;
 
 require('readline').createInterface({
     input: process.stdin,
     output: process.stdout
 }).on('line', function (line) {
+    guessNumberProcess(line);
+});
+
+function guessNumberProcess(line) {
     const input = line.split(" ");
+    let end = false;
     if (!verifyInput(input)) {
         console.log(ERROR_MESSAGE);
     } else {
         const result = checkAnswer(input);
         console.log(`${result.A}A${result.B}B`);
+        chances--;
+        if (chances === 0 || result.A === 4) {
+            end = true;
+        }
     }
-});
+    if (end) {
+        console.log("Game over.");
+        process.exit();
+    }
+}
 
 function verifyInput(input) {
     if (new Set(input).size == 4) {
