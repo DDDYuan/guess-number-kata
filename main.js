b767d1302aa887615441fb5f642a9072d1e59e26
Di Yuan <dyuan@thoughtworks.com>
2018-07-18 15:31:34 +0800

    [Yuan Di] task2 - output 0A0B for validated input & error message for invalidated input

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
 "use strict";
 
+const ERROR_MESSAGE = "Wrong Input，Input again";
+
 require('readline').createInterface({
     input: process.stdin,
     output: process.stdout
 }).on('line', function (line) {
-    console.log("0A0B");
+    if (!verifyInput(line)) {
+        console.log(ERROR_MESSAGE);
+    } else {
+        console.log("0A0B");
+    }
 });
+
+function verifyInput(input) {
+    if (new Set(input.split(" ")).size == 4) {
+        return input.split(" ").every(num => {
+            var number = parseInt(num);
+            return number >= 0 && number <= 9;
+        });
+    }
+    return false;
+}
